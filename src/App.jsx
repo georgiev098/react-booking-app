aeede852e6bd3f49fea9c4e036c73516024a5260
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-11-20 13:07:48 +0200

    (style) include styled components and begin intial styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 export default function App() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Booking Oasis</H1>
+        <Button>Click</Button>
+      </div>
+    </>
+  );
 }
