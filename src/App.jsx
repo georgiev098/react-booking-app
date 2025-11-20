4417fc3f865cf8db16d295353bca9a2d79778b03
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-11-20 13:21:22 +0200

    (style) building a reusable heading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <div>
-        <H1>Booking Oasis</H1>
+        <Heading as="h1">Booking Oasis</Heading>
         <Button>Click</Button>
       </div>
     </>
