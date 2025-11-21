0844c33806cf7cefbdb775d44ca9bbdb1bc24ddc
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-11-21 16:34:33 +0200

    (feat) implement initial supabase connection

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 export default function Cabins() {
+  async function fetchCabins() {
+    const data = await getCabins();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchCabins();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
