b8e834e1345bf7e6fbc938a2a0ab56acd0c4a9ef
Daniel Georgiev <georgiev.daniel@outlook.com>
2025-11-26 14:54:50 +0200

    (feat) implement toast notifications

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabinById } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -60,10 +61,10 @@ export default function CabinRow({ cabin }) {
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
+
+      toast.success(" Cabin deleted successfully");
     },
-    onError: (err) => {
-      console.error(err);
-    },
+    onError: (err) => toast.error(err.message),
   });
 
   return (
